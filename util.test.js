b9fce283075839b2cd906f84f2571c4e42e056dd
safeamiiir <safehian_amirreza@yahoo.com>
2020-03-18 16:08:11 +0330

    handle puppeteer test for e2e

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer')
 const { generateText, checkAndGenerate } = require("./util");
 
 test("output title and price", () => {
@@ -9,3 +10,18 @@ test("Check and generate input", () => {
   const text = checkAndGenerate("Book", 79);
   expect(text).toBe("Book 79");
 });
+
+test('clicked', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size=1920,1080']
+  })
+  const page = await browser.newPage()
+  await page.goto('http://127.0.0.1:8080/')
+  await page.click('#title')
+  await page.type('#title', 'Book')
+  await page.click('#price')
+  await page.type('#price', '79')
+  await page.click('#add-product')
+})
